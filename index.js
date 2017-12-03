ec67dedf79842fbb6769e717cf133388500aefdf
RytardCodes <33573018+RytardCodes@users.noreply.github.com>
2017-12-03 20:08:30 +0800

    Just a simple ping pong extended Command

---
+++ index.js
@@ -0,0 +1,14 @@
+var Eris = require('eris'); // Define the Eris Library
+var bot = new Eris(); // Token Type: String
+
+bot.on('ready', () => { // Same as D.js Ready Event
+    console.log('Ready!')
+});
+
+bot.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        bot.createMessage(message.channel.id, `Pong! Took \`${message.channel.guild.shard.latency}\``) // If the Message is ping, reply with the API Latency and Pong!
+    }
+});
+
+bot.connect(); // Connect the Bot to Discord!
