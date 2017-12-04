0a766860b427832ef778691abecf3eebdbebca34
RytardCodes <33573018+RytardCodes@users.noreply.github.com>
2017-12-04 19:53:42 +0800

    Update for Command-c

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-var Eris = require('eris'); // Define the Eris Library
-var bot = new Eris(); // Token Type: String
+:pushpin: These Rules Apply to Every User on the Discord Chat ( Guild )
+Anyone found Breaking these Rules will get a Punishment.
 
-bot.on('ready', () => { // Same as D.js Ready Event
-    console.log('Ready!')
-});
+Current Rules for __*Every User Except Staff*__
 
-bot.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        bot.createMessage(message.channel.id, `Pong! Took \`${message.channel.guild.shard.latency}\``) // If the Message is ping, reply with the API Latency and Pong!
-    }
-});
+**1.** Don't be a Jerk to Anyone, or Piss them off.
 
-bot.connect(); // Connect the Bot to Discord!
+**2.** Follow the Channel Names, as the name was made for a certain topic.
+
+**3.** No advertising in Any Channels!
+
+**4.** Talking about how horrible/trash talking the bot is not allowed in this Discord Chat!
+
+**5.** Everyone have Permissions to use/edit the Github Bot, just remember to give us Credit! ;)
+
+**6.** If you need help on Something, please go to the Support Channel #support 
+
+**7.** Need help? And no on is responding in 5 Minutes? Feel free to mention the @Helpers Role! ( No Spamming )
+
+**8.** No spamming in any Channels.
+
+**9.** Please don't beg for Help, like "`How can i do this or that? Any Code you Can Give?`" Will result in Punishments.
+
+**10.** If your name isn't mentionable ( E.g `ᐃᑦᑎᓂᖅᓯᐅᑐᖅ ᑕᖅᓴᖅ` ). Please Change it to a proper nickname we can mention.
